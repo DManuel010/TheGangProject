7b909b80922b70bac845392f9e5189808ddf4de7
simpsona4 <simpsona4@wit.edu>
2020-01-31 22:36:14 -0500

    Feature: 1. Cleaned up App.js to only include the React render function of each component. 2. App.cs contains all of the aesthetic design for the landing-page above the 'Hello World' and React button.  App.cs was updated at some point to make the zoom animation slower and the same speed when going in and out.  --Makes the zoom easier on the eyes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
-import logo from "./assets/thegang.jpg";
+import WebsiteBG from "./components/WebsiteBG";
+import WebsitePC from "./components/WebsitePC";
+import WebsiteScrollbar from "./components/WebsiteScrollbar"
+import WebsiteText from "./components/WebsiteText";
+import Clock from "./components/Clock";
+import Toggle from "./components/Toggle";
 import "./App.css";
 import Button from "./components/Button";
 import Div from "./components/DynamicDiv";
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+          <WebsiteBG />
+          <WebsitePC />
+          <WebsiteScrollbar />
+          <WebsiteText />
+          <Clock />
+          <Toggle />
+        <header className="App-header">
         <div className="LearnReact">learn react</div>
-
-        <img src={logo} className="App-logo" alt="logo" />
 
         <Button
           style={{ marginTop: 128 }}

50cb036acb83309206b7995193bb1b38aa276d74
simpsona4 <simpsona4@wit.edu>
2020-01-31 22:28:21 -0500

    Feature: Added a mock-up header, paragraph, slogan, and a responsive HTML link button that takes you to a React tutorial.

---
+++ frontend/src/components/WebsitePC/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const WebsitePC = () => {
+    return (
+        <div className="page-content">
+            <h1>Smite Builder</h1>
+            <p>
+                Smite Builder is a comprehensive, next-gen team building software that utilizes SMITE.API and it's underlying algorithms to determine the best team composition for you.  The team chosen for you can be based on your preferred gods / personal god performance, the enemy team composition and draft process, and even the overall community's god statistics.  Don't worry about a thing.  Your smite games will be won because you listened to us.
+            </p>
+            <h2> Draft. Pick. Profit. </h2>
+            <a href="https://reactjs.org/tutorial/tutorial.html" target="_blank" rel="noopener noreferrer">Get Started</a>
+        </div>
+    );
+}
+
+export default WebsitePC

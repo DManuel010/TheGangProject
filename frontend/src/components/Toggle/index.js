eae49f322ce86e85041068ab1d6c59adda460382
simpsona4 <simpsona4@wit.edu>
2020-01-31 22:23:38 -0500

    Feature: Added a React button that toggles On/Off.  --No use.

---
+++ frontend/src/components/Toggle/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <div style={{
+                backgroundColor: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center'
+            }}>
+                <button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? 'ON' : 'OFF'}
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Toggle

3ea0cc4a9f02c1b3afc25142a7c667156d8fd240
simpsona4 <simpsona4@wit.edu>
2020-01-31 22:31:29 -0500

    Feature: Added my own 'lorem ipsum' text below the main landing-page background to see the aesthetic of the scrollbar and a longer page more apparently.

---
+++ frontend/src/components/WebsiteText/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const WebsiteText = () => {
+    return (
+        <div className="text">
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+            <div>"There are two kinds of people.  Those who don't believe in miracles, and those who believe that everything is a miracle.  And I, for one, am the latter."  - Albert Einstein</div>
+        </div>
+    );
+}
+
+export default WebsiteText

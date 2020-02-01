3452180f94c5664cb21d4a17559f981242b3a32f
Drew <d.manuel010@gmail.com>
2020-01-31 23:28:40 -0500

    feat: Added DynamicDiv to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,24 @@ import React from "react";
 import logo from "./assets/thegang.jpg";
 import "./App.css";
 import Button from "./components/Button";
+import Div from "./components/DynamicDiv";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="LearnReact">learn react</div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <Button
-          style={{ marginTop: 128 }}
-          onClick={() => {
-            window.alert("Me Me Big Boi Gang");
-          }}
-        >
+
+        <Button 
+          style={{ marginTop: 128 }} 
+          onClick={() => { 
+            window.alert("Me Me Big Boi Gang"); 
+          }}>
           Click Me
         </Button>
+        <Div style={{ marginTop: 20 }}></Div>
       </header>
     </div>
   );

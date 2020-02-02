29a9a6d4db22a64e8826a7bc3418878a6f12c726
Dat Do <dat.b.do@gmail.com>
2020-02-02 18:06:14 -0500

    feature: intergrate react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,21 @@
 import React from "react";
-import WebsiteBG from "./components/WebsiteBG";
-import WebsitePC from "./components/WebsitePC";
-import WebsiteScrollbar from "./components/WebsiteScrollbar";
-import WebsiteText from "./components/WebsiteText";
-import Clock from "./components/Clock";
-import Toggle from "./components/Toggle";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
-import Button from "./components/Button";
-import Div from "./components/DynamicDiv";
+import routes, { errorRoutes } from "./routes";
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <WebsiteBG />
-      <WebsitePC />
-      <WebsiteScrollbar />
-      <WebsiteText />
-      <Clock />
-      <Toggle />
-      <header className="App-header">
-        <div className="LearnReact">dont learn react</div>
-
-        <Button
-          style={{ marginTop: 128 }}
-          onClick={() => {
-            window.alert("Me Me Big Boi Gang");
-          }}
-        >
-          Click Me
-        </Button>
-        <Div style={{ marginTop: 20, color: "#ff7979" }} />
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {routes.map(route => (
+          <Route key={route.name} component={route.component} path={route.path} exact={route.exact} />
+        ))}
+        {errorRoutes.map(route => (
+          <Route key={route.name} component={route.component} path={route.path} exact={route.exact} />
+        ))}
+      </Switch>
+    </Router>
   );
 }
 

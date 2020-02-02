06506316921e3fe34537ced1d83c0649213de8a6
Dat Do <dat.b.do@gmail.com>
2020-02-02 18:06:25 -0500

    tests: update test to new structure

--- frontend/src/__tests__/containers/Home/Home.test.js
+++ frontend/src/__tests__/containers/Home/Home.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "../App";
+import Home from "../../../containers/Home";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<Home />);
+  const linkElement = getByText(/The Gang Project/i);
   expect(linkElement).toBeInTheDocument();
 });

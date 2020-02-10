9964050ecc472bdcbfd2e9dddf50d3ff1369a152
simpsona4 <simpsona4@wit.edu>
2020-02-10 15:22:52 -0500

    feature: Added a dynamic blur effect to the background image when scrolling.  chore: moved landing-page css into WebsiteBG component; removed unused css code

--- frontend/src/components/WebsiteBG/index.js
+++ frontend/src/components/WebsiteBG/index.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
-const WebsiteBG = () => <div className="landing-page" />;
+const WebsiteBG = () => {
+  //Better version --based on scroll event instead of looped requests
+  const [pos, setPos] = useState(window.pageYOffset); // eslint-disable-line
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setPos(window.pageYOffset * 2.75);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  var blurNum = Math.min(10, pos / 100);
+
+  return (
+    <div class="back">
+      <div class="landing-page" style={{ filter: `blur(${blurNum}px)` }} />
+    </div>
+  );
+};
 
 export default WebsiteBG;

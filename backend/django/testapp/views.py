47dee21eb9afe75b4f8e3964828f37f004118906
Dat <dat.b.do@gmail.com>
2020-02-12 22:53:39 -0500

    docs: add documentation for root route in testapp

--- backend/django/testapp/views.py
+++ backend/django/testapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
-# Create your views here.
+# 'methods' can be used to apply the same modification to multiple methods
+@swagger_auto_schema(methods=['get'], responses={200: "Ya good"})
+@api_view(["GET"])
 def index(request):
     return HttpResponse("Hello world! This is the testapp index.")

07bbc578aae4ce579e1ba5bb145c25621099e7b2
Dat <dat.b.do@gmail.com>
2020-02-12 22:53:52 -0500

    feature: create new app for smitegang api

---
+++ backend/django/smitegang/views/index.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+# 'methods' can be used to apply the same modification to multiple methods
+@swagger_auto_schema(methods=['get'], responses={200: "Ya good"})
+@api_view(["GET"])
+def index(request):
+    """
+    The Gang's Smite Root API
+    """
+    return HttpResponse("Hello world! This is the smitegang index.")

2bbdcc66d0c9cee7bc48fde30ab52bbe26e834f2
Dat <dat.b.do@gmail.com>
2020-04-09 20:03:55 -0400

    feat: add firebase context to react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import routes, { errorRoutes } from "./routes";
+import { FirebaseContextProvider } from "./context/FirebaseContext";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        {routes.map(route => (
-          <Route key={route.name} component={route.component} path={route.path} exact={route.exact} />
-        ))}
-        {errorRoutes.map(route => (
-          <Route key={route.name} component={route.component} path={route.path} exact={route.exact} />
-        ))}
-      </Switch>
-    </Router>
+    <FirebaseContextProvider>
+      <Router>
+        <Switch>
+          {routes.map(route => (
+            <Route key={route.name} component={route.component} path={route.path} exact={route.exact} />
+          ))}
+          {errorRoutes.map(route => (
+            <Route key={route.name} component={route.component} path={route.path} exact={route.exact} />
+          ))}
+        </Switch>
+      </Router>
+    </FirebaseContextProvider>
   );
 }
 
